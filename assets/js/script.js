d2c3ed01a2e44cb9fc439df5120f8ad1b8228886
jennasaikaly <jen.saikaly@gmail.com>
2022-03-11 12:53:55 -0500

    added initial prompt

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,37 @@
 // Assignment code here
 
 
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+// 1) click on the button, recieve series of prompts
+// 2) prompt #1 special characters   " !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
+// 4) prompt #2  length of password   at least 8 characters and no more than 128 characters
+//5)  prompt #2 include lowercase, uppercase, numeric, and/or special characters
+
+// 6) user has to confirm selection; at least 1 character must be chosen;
+
+//7)  all prompts must be answered ; then a password is created that matches selections
+//8) password is displayed as an alert or on the page;
+
+
+
+
+  
+
+
 
 // Write password to the #password input
 function writePassword() {
+  window.prompt("Welcome to Password Generator! To get started, please choose how long you would like your pw to be.");
+  
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
 }
 
+function generatePassword(){
+}
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
