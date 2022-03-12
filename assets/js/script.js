043d9cdb5e7f95c381e146cadac647fe0aed4955
jennasaikaly <jen.saikaly@gmail.com>
2022-03-12 10:07:11 -0500

    added prompt variables and functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,9 +19,72 @@
 
 
 // Write password to the #password input
-function writePassword() {
-  window.prompt("Welcome to Password Generator! To get started, please choose how long you would like your pw to be.");
+
+var lengthCharacter = function() {
+  var lengthPrompt = window.prompt(
+    "Welcome to Password Generator! To get started, please choose how long you would like your Password (8-128 characters) to be."
+    ); 
+    if (!lengthPrompt) {
+      window.alert("Please make a selection!");
+      return lengthCharacter();
+     }
+}
+
+var numberCharacter = function() {
+  var numberPrompt = window.prompt(
+    "Would you like to include NUMBERS? Please press 1 for YES or 2 for NO"
+    );
+    if (!numberPrompt) {
+      window.alert("Please make a selection!");
+      return numberCharacter();
+    }
+}
+
+var upperCaseCharacter = function() {
+  var upperCasePrompt = window.prompt(
+    "Would you like to include UPPER-CASE LETTERS? Please press 1 for YES or 2 for NO"
+    );
+    if (!upperCasePrompt) {
+      window.alert("Please make a selection!");
+      return upperCaseCharacter();
+  }
+}
+
+var lowerCaseCharacter = function() {
+  var lowerCasePrompt = window.prompt(
+    "Would you like to include LOWER-CASE LETTERS? Please press 1 for YES or 2 for NO"
+    );
+    if (!lowerCasePrompt) {
+      window.alert("Please make a selection!");
+      return lowerCaseCharacter();
+  }
+}
+
+var specialCharacter = function() {
+  var specialCharPrompt = window.prompt(
+    "Would you like to include a SPECIAL CHARACTER? Please press 1 for YES or 2 for NO"
+    );
+    if (!specialCharPrompt) {
+      window.alert("Please make a selection!");
+      return specialCharacter();
+  }
+}
+
+
+
+
   
+  
+function writePassword() {
+      lengthCharacter();
+      numberCharacter();
+      upperCaseCharacter();
+      lowerCaseCharacter();
+      specialCharacter();
+
+  
+
+ 
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
@@ -34,4 +97,4 @@ function generatePassword(){
 var generateBtn = document.querySelector("#generate");
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
+characterLength(); 
