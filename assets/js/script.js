8e047e38e57fb7b760d444d3b57fe3078512e546
jennasaikaly <jen.saikaly@gmail.com>
2022-03-13 23:43:37 -0400

    added condition that at least one character type must be chosen and cleaned up code

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,118 +17,120 @@
 
 
 /******************************/
- // GENERATE PASSWORD FUNCTION
+// GENERATE PASSWORD FUNCTION
 /*****************************/
 function generatePassword() {
 
   //debugger;
 
- /********************************/
-//      Gather Information
-/********************************/
+  /********************************/
+  //      Gather Information
+  /********************************/
+  var confirmChoices = true;
 
-  var passwordLengthChoice = window.prompt(
-    "Welcome to Password Generator! To get started, please choose how long you would like your Password (8-128 characters) to be."
-    ); 
-    
-    if (!passwordLengthChoice || passwordLengthChoice < 8 || passwordLengthChoice > 128) {
-    passwordLengthChoice = window.prompt("You made an incorrect selection.  Please choose a number between 8 and 128.");
-    } 
-  
-   else {
-   var upperCaseChoice= window.confirm(
-    "Would you like to include UPPER-CASE CHARACTERS? Please press OK for YES or CANCEL for NO"
+  while (confirmChoices) {
+    var passwordLengthChoice = window.prompt(
+      "Welcome to Password Generator! To get started, please choose how long you would like your Password (8-128 characters) to be."
     );
-    
 
-  var lowerCaseChoice = window.confirm(
-    "Would you like to include LOWER-CASE LETTERS? Please press OK for YES or CANCEL for NO"
+    while (!passwordLengthChoice || passwordLengthChoice < 8 || passwordLengthChoice > 128) {
+      passwordLengthChoice = window.prompt("You made an incorrect selection.  Please choose a number between 8 and 128.");
+    };
+
+    var upperCaseChoice = window.confirm(
+      "Would you like to include UPPER-CASE CHARACTERS? Please press OK for YES or CANCEL for NO"
     );
-    
-  var numberChoice = window.confirm(
-    "Would you like to include NUMBERS?Please press OK for YES or CANCEL for NO"
+
+
+    var lowerCaseChoice = window.confirm(
+      "Would you like to include LOWER-CASE LETTERS? Please press OK for YES or CANCEL for NO"
     );
-   
-  
-  var specialCharacterChoice = window.confirm(
-    "Would you like to include a SPECIAL CHARACTER? Please press OK for YES or CANCEL for NO"
+
+    var numberChoice = window.confirm(
+      "Would you like to include NUMBERS?Please press OK for YES or CANCEL for NO"
     );
-    
-  }       
-/**************************/
-//CONFIRM CHOICES
-/**************************/
-//if they select "confirm" within the confirm window
-//selectionApproved = true;
-//selectionDenied = false;
-//
-var confirmChoices = window.confirm(
-  "Please confirm your choices! "  + 
-  "\nUppercase enabled: " + upperCaseChoice + 
-  "\nLowercase enabled: " + lowerCaseChoice +
-  "\nNumbers enabled: " + numberChoice +
-  "\nSpecial Characters enabled: " + specialCharacterChoice +
-  "\nLength of password: " + passwordLengthChoice
-);
-if (confirmChoices === false) {
-  window.alert("Please enter your selections again.");
-  generatePassword();
-}
-debugger;     
-/************************************/
-//    ARRAY CODE
-/************************************/
 
-characterSelectionArray = [];
-
-upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-numbers = ["1","2","3","4","5","6","7","8","9","0"];
-specialCharacters = ["!","\"","#","$","%","&","'","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","\\","]","^","_","`","{","|","}","~"];
+    var specialCharacterChoice = window.confirm(
+      "Would you like to include a SPECIAL CHARACTER? Please press OK for YES or CANCEL for NO"
+    );
 
 
-if (upperCaseChoice){
-  characterSelectionArray = characterSelectionArray.concat(upperCase);
-   
-}
+    /**************************/
+    //CONFIRM CHOICES
+    /**************************/
+    //if they select "confirm" within the confirm window
+    //selectionApproved = true;
+    //selectionDenied = false;
+    //
 
-if (lowerCaseChoice){
-  characterSelectionArray = characterSelectionArray.concat(lowerCase);
-    
+    userConfirmChoices = window.confirm(
+      "Please confirm your choices! " +
+      "\nUppercase enabled: " + upperCaseChoice +
+      "\nLowercase enabled: " + lowerCaseChoice +
+      "\nNumbers enabled: " + numberChoice +
+      "\nSpecial Characters enabled: " + specialCharacterChoice +
+      "\nLength of password: " + passwordLengthChoice
+    );
 
-}
+    if (userConfirmChoices === false) {
+      window.alert("Please enter your selections again.");
+    } else if (!upperCaseChoice && !lowerCaseChoice && !numberChoice && !specialCharacterChoice) {
+      window.alert("Please enter your selections again. You must select at least one character class!");
+    } else {
+      confirmChoices = false;
+    }
 
-if (numberChoice){
-  characterSelectionArray = characterSelectionArray.concat(numbers);
-}
+  }
+  /************************************/
+  //    ARRAY CODE
+  /************************************/
 
-if (specialCharacterChoice){
-  characterSelectionArray = characterSelectionArray.concat(specialCharacters);
-} 
+  characterSelectionArray = [];
 
-/*************************/
+  const upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  const lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  const numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+  const specialCharacters = ["!", "\"", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"];
+
+
+  if (upperCaseChoice) {
+    characterSelectionArray = characterSelectionArray.concat(upperCase);
+  }
+
+  if (lowerCaseChoice) {
+    characterSelectionArray = characterSelectionArray.concat(lowerCase);
+  }
+
+  if (numberChoice) {
+    characterSelectionArray = characterSelectionArray.concat(numbers);
+  }
+
+  if (specialCharacterChoice) {
+    characterSelectionArray = characterSelectionArray.concat(specialCharacters);
+  }
+
+  /*************************/
   //GENERATE PASSWORD
-/*************************/
+  /*************************/
 
-var pwd ="";
-for(i = 0; i < passwordLengthChoice; i++) {
-  const random = characterSelectionArray[Math.floor(Math.random() * characterSelectionArray.length)];
-  pwd += random
-}
-return pwd;
+  var password = "";
+  for (i = 0; i < passwordLengthChoice; i++) {
+    password += characterSelectionArray[Math.floor(Math.random() * characterSelectionArray.length)];
+  }
+  console.log(password);
+  return password;
 
 
-  
 }
 /**********************/
 // WRITE PASSWORD FUNCTION 
 /**********************/
 // Write password to the #password input
-function writePassword(){
-  
-  var password = generatePassword();
+function writePassword() {
+
+  var generatedPassword = generatePassword();
   var passwordText = document.querySelector("#password");
-  passwordText.value = password;
+  passwordText.value = generatedPassword;
 }
 
 
